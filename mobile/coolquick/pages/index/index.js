9e21a242816dd2bbef74bd4293ced183169fbfde
kaiba <407703712@gmail.com>
2018-05-21 17:46:24 +0800

    add new page

--- mobile/coolquick/pages/index/index.js
+++ mobile/coolquick/pages/index/index.js
@@ -4,83 +4,72 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    dates: '2016-11-08',
-    times: '12:00',
-    objectArray: ['中国', '英国', '美国'],
-    index: 0,
+    imgUrls: [
+      '../images/home_img_banner.png',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
+    circular:true,
+    showLayer:false,
+    phoneLogo:'../images/home_img_mobile.png',
+    phoneModel:"iPhone 7",
+    faultArr:["屏幕故障","升级内存","屏幕故障","升级内存","屏幕故障","升级内存","屏幕故障","升级内存",],
+    showMoney:"￥0.00",
+    isYuyue:false,
+    items: [
+      {name: 'USA', content: '超出了上门维修的范围',price:"￥99.00"},
+      {name: 'CHN', content: '下错单了',price:"￥99.00"},
+      {name: 'BRA', content: '不想到店维修',price:"￥99.00"},
+      {name: 'JPN', content: '等待时间太久',price:"￥99.00"},
+      {name: 'ENG', content: '设备已恢复正常',price:"￥99.00"},
+      {name: 'TUR', content: '不放心取机维修',price:"￥99.00"},
+    ],
+    descriptsTxt:"1：大阿斯达阿斯达阿斯达哇所多阿斯达䦺地方水电费；2：何贵何贱同一句话铁公鸡一发过火头发；3：电饭锅热点覆盖人地方电饭锅帝国电饭锅",
   },
-  //  点击时间组件确定事件  
-  bindPickerChange: function (e) {
-    console.log(e);
-    this.setData({
-      index: e.detail.value
-    })
-  },
-  //  点击时间组件确定事件  
-  bindTimeChange: function (e) {
-    console.log("谁哦按")
-    this.setData({
-      times: e.detail.value
-    })
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.switchTab({
-      url: '../logs/logs',
-      fail:function(e){
-        console.log(e);
+  checkboxChange: function(e) {
+    var self=this;
+    var arrs=this.data.items;
+    var lastName=e.detail.value[e.detail.value.length-1];
+    for (var x in arrs) {
+      if (lastName==arrs[x].name) {
+        arrs[x]['checked']=true;
+      }else{
+        arrs[x]['checked']=false;
       }
-    })
-  },
-  onLoad: function () {
-    console.log("1");
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
     }
-    const self=this;
-    wx.request({
-      url:"https://api.zikang123.com/reading/banner?project=1",
-      success:function(res){
-        console.log(res);
-        self.setData({
-          motto: res.data.datas
-        })
-      }
+    self.setData({
+          items:arrs,
+    })
+  },    
+  closeLayer:function(){
+    this.setData({
+      showLayer:false
     })
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  showLayer:function(){
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      showLayer:true
+    })
+  },  
+  goSelect:function(){
+    wx.navigateTo({
+      url:'../phoneModel/phoneModel'
+    });
+    wx.setNavigationBarTitle({
+      title: '选择机型'//页面标题为路由参数
+    })
+  },
+  goPlaceOrder:function(){
+    wx.navigateTo({
+      url:'../placeOrder/placeOrder'
+    });
+    wx.setNavigationBarTitle({
+      title: '维修方案'//页面标题为路由参数
     })
   }
+ 
 })
