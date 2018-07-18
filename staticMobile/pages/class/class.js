ac3863b979be0f4d030a0a21b9422be81fc5bd3c
haimalairen <407703712@qq.com>
2018-07-19 00:06:08 +0800

    7/19 home
    kaiba

---
+++ staticMobile/pages/class/class.js
@@ -0,0 +1,21 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    listDatas:[
+      {'img':"../images/img19.jpg",'title':'刘思高','content':'在讨论组中提交了新的观点','time':'刚刚'},
+      {'img':"../images/img19.jpg",'title':'刘思高','content':'完成了本日作业，获得98分','time':'20分钟前'},
+      {'img':"../images/img19.jpg",'title':'刘思高','content':'完成了本日作业，获得97分','time':'3小时前'},
+      {'img':"../images/img19.jpg",'title':'刘思高','content':'加入了新讨论组','time':'1天前'},
+      {'img':"../images/img19.jpg",'title':'刘思高','content':'在讨论组中提交了新的观点','time':'1天前'},
+      {'img':"../images/img19.jpg",'title':'刘思高','content':'在讨论组中提交了新的观点','time':'1天前'},
+      {'img':"../images/img19.jpg",'title':'刘思高','content':'在讨论组中提交了新的观点','time':'1天前'},
+      {'img':"../images/img19.jpg",'title':'刘思高','content':'在讨论组中提交了新的观点','time':'1天前'},
+    ]
+  },
+  onLoad: function () {
+
+  }
+})
---
+++ staticMobile/pages/find/find.js
@@ -0,0 +1,21 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    listDatas:[
+      {'img':"../images/img19.jpg",'title':'课程标题','content':'课程简介课程简介课程简介课程简介课程简介。','studynum':'4553'},
+      {'img':"../images/img19.jpg",'title':'课程标题','content':'课程简介课程简介课程简介课程简介课程简介。','studynum':'4553'},
+      {'img':"../images/img19.jpg",'title':'课程标题','content':'课程简介课程简介课程简介课程简介课程简介。','studynum':'4553'},
+      {'img':"../images/img19.jpg",'title':'课程标题','content':'课程简介课程简介课程简介课程简介课程简介。','studynum':'4553'},
+      {'img':"../images/img19.jpg",'title':'课程标题','content':'课程简介课程简介课程简介课程简介课程简介。','studynum':'4553'},
+      {'img':"../images/img19.jpg",'title':'课程标题','content':'课程简介课程简介课程简介课程简介课程简介。','studynum':'4553'},
+      {'img':"../images/img19.jpg",'title':'课程标题','content':'课程简介课程简介课程简介课程简介课程简介。','studynum':'4553'},
+      {'img':"../images/img19.jpg",'title':'课程标题','content':'课程简介课程简介课程简介课程简介课程简介。','studynum':'4553'},
+    ]
+  },
+  onLoad: function () {
+
+  }
+})
