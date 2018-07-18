4ad83735822b8c2c24965e13890aa2d6ff9e5d14
kaiba <407703712@gmail.com>
2018-07-18 17:57:53 +0800

    company 7/18
    kaiba

---
+++ staticMobile/pages/index/index.js
@@ -0,0 +1,19 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    indicatorDots: true,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
+    circular:true,
+    indicatorColor:"#C4C4C4",
+    indicatorActiveColor:"#1B7ED5",
+    swiperItems:[{'img':'../images/banner1.png'},{'img':'../images/banner1.png'},{'img':'../images/banner1.png'}]
+  },
+  onLoad: function () {
+
+  }
+})
