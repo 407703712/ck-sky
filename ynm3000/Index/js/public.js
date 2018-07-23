3112ced9425b538455caaa707a0f33fc089e0226
kaiba <407703712@gmail.com>
2018-07-23 18:00:48 +0800

    7/23 company
    kaiba

--- ynm3000/Index/js/public.js
+++ ynm3000/Index/js/public.js
@@ -6,20 +6,15 @@ $(function(){
 	function dropW(){
 		var bodyW=$(".container").width();
 		$(".drop-kaiguan").width(bodyW);
-		$(".second-brand").click(function(event) {
+		$(".m-default-search-btn").click(function(event) {
 			/* Act on the event */
-			event.stopPropagation();
-			$(this).find('.m-default-search-btn').toggleClass('msearch-active-btn');
+			$(this).toggleClass('msearch-active-btn');
 			$(".blue-sh").toggle();
 			$(".drop-kaiguan").toggle();
 		});
 	}
 
 	dropW();
-	$(".drop-kaiguan").click(function(event) {
-		/* Act on the event */
-		event.stopPropagation();
-	});
 	$(window).resize(function(event) {
 		/* Act on the event */
 		dropW();
